c3df668913d9930f9cfd16fdd81babc0edf7c79f
siddharthashandilya <siddharthashandilya104@gmail.com>
2022-11-10 09:09:55 +0530

    updated app.py file for text summarization

--- app.py
+++ app.py
@@ -1,12 +1,61 @@
-from flask import Flask
+import os
+from flask import Flask, flash, request, redirect, render_template
+from werkzeug.utils import secure_filename
+from src.readFile import read_file
+from src.tsummarizer import summarizer
 
 app=Flask(__name__)
 
-
-@app.route("/",methods=['GET','POST'])
-def index():
-    return "CI CD pipeline has been established for text-summary app ."
+app.secret_key = "secret key"
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024
 
 
-if __name__=="__main__":
-    app.run(debug=True)
+path = os.getcwd()
+# file Upload
+UPLOAD_FOLDER = os.path.join(path, 'uploads')
+
+if not os.path.isdir(UPLOAD_FOLDER):
+    os.mkdir(UPLOAD_FOLDER)
+
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
+ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
+
+
+def allowed_file(filename):
+    return '.' in filename and filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
+
+@app.route('/')
+def upload_form():
+    return render_template('upload.html')
+
+
+@app.route('/', methods=['POST'])
+def upload_file():
+    if request.method == 'POST':
+        # check if the post request has the file part
+        if 'file' not in request.files:
+            flash('No file part')
+            return redirect(request.url)
+        file = request.files['file']
+        if file.filename == '':
+            flash('No file selected for uploading')
+            return redirect(request.url)
+        if file and allowed_file(file.filename):
+            filename = secure_filename(file.filename)
+            saved_img_path = file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
+            flash('File successfully uploaded')
+            img_text = read_file(image_path=saved_img_path)
+            text_summary = summarizer(text = img_text)
+            return text_summary
+
+            #return redirect('/')
+        else:
+            flash('Allowed file types are txt, pdf, png, jpg, jpeg, gif')
+            return redirect(request.url)
+
+
+if __name__ == "__main__":
+    app.run(host = '127.0.0.1',port = 5000, debug = True)
